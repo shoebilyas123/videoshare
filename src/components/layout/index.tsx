50499cd718ffba6b96d4a961e72726ec7566fe02
shoebilyas123 <shoebilyas123@gmail.com>
2023-06-30 15:21:55 +0530

    feat: sidebar init

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,11 +1,87 @@
-import React, { FC } from 'react';
+import React, { useState } from 'react';
+import {
+  AiOutlineUser,
+  AiOutlinePieChart,
+  AiOutlineDesktop,
+  AiOutlineTeam,
+  AiOutlineFile,
+} from 'react-icons/ai';
+import type { MenuProps } from 'antd';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
 
-interface LayoutProps {
-  children?: any;
+const { Header, Content, Footer, Sider } = Layout;
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[]
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  } as MenuItem;
 }
 
-const Layout: FC<LayoutProps> = ({ children }) => {
-  return <div className="w-screen h-screen overflow-hidden">{children}</div>;
+const items: MenuItem[] = [
+  getItem('Option 1', '1', <AiOutlinePieChart />),
+  getItem('Option 2', '2', <AiOutlineDesktop />),
+  getItem('User', 'sub1', <AiOutlineUser />, [
+    getItem('Tom', '3'),
+    getItem('Bill', '4'),
+    getItem('Alex', '5'),
+  ]),
+  getItem('Team', 'sub2', <AiOutlineTeam />, [
+    getItem('Team 1', '6'),
+    getItem('Team 2', '8'),
+  ]),
+  getItem('Files', '9', <AiOutlineFile />),
+];
+
+const AppLayout: React.FC = () => {
+  const [collapsed, setCollapsed] = useState(true);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  return (
+    <Layout style={{ minHeight: '100vh', minWidth: '100vw' }}>
+      <Sider collapsible collapsed={collapsed}>
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          defaultSelectedKeys={['1']}
+          mode="inline"
+          items={items}
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }} />
+        <Content style={{ margin: '0 16px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+              background: colorBgContainer,
+            }}
+          >
+            Bill is a cat.
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Ant Design ©2023 Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
+  );
 };
 
-export default Layout;
+export default AppLayout;
